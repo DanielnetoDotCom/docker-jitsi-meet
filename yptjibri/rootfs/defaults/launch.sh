643bb8e98833060cf1b058fd7ca3f5968a3a8670
DanielnetoDotCom <danielneto.com@gmail.com>
2020-07-30 20:41:20 -0300

    ypt

--- yptjibri/rootfs/defaults/launch.sh
+++ yptjibri/rootfs/defaults/launch.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+export PATH="/opt/utils:$PATH"
+
 exec java -Djava.util.logging.config.file=/etc/jitsi/jibri/logging.properties -jar /opt/jitsi/jibri/jibri.jar --config "/etc/jitsi/jibri/config.json"  --internal-http-port 3333{{ .Env.JIBRI_INSTANCE }}  --http-api-port 2222{{ .Env.JIBRI_INSTANCE }}
